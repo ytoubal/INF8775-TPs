95f5874da3c988d222099c2c7d375e1b597849f4
Yanis Toubal <yanis.toubal@polymtl.ca>
2021-10-18 14:42:39 -0400

    refactor code

--- TP1-A21/main.py
+++ TP1-A21/main.py
@@ -2,10 +2,11 @@
 import matrix_parser
 import conventional
 import argparse
-import strassen1
+import strassen
 import strassen_sueil
 import numpy as np
 from timeit import default_timer as timer
+import time
 
 
 if __name__ == "__main__":
@@ -35,8 +36,8 @@ if __name__ == "__main__":
     print(show_result)
     print(show_time)
 
-    matrix_parser.parse(r"/Users/yacinehamdani/Desktop/SessionA21-etranger/cours/inf8775/TP1-A21/INF8775-TPs/TPs/TP1-A21/data/ex2_0",r"/Users/yacinehamdani/Desktop/SessionA21-etranger/cours/inf8775/TP1-A21/INF8775-TPs/Tps/TP1-A21/data/ex2_1")
-    # matrix_parser.parse(r"C:\Users\yanis\Desktop\Poly\A2021\INF8775\TPs\TP1-A21\data\ex8_0",r"C:\Users\yanis\Desktop\Poly\A2021\INF8775\TPs\TP1-A21\data\ex8_1")
+    # matrix_parser.parse(r"/Users/yacinehamdani/Desktop/SessionA21-etranger/cours/inf8775/TP1-A21/INF8775-TPs/TPs/TP1-A21/data/ex2_0",r"/Users/yacinehamdani/Desktop/SessionA21-etranger/cours/inf8775/TP1-A21/INF8775-TPs/Tps/TP1-A21/data/ex2_1")
+    matrix_parser.parse(r"C:\Users\yanis\Desktop\Poly\A2021\INF8775\TPs\TP1-A21\data\ex8_0",r"C:\Users\yanis\Desktop\Poly\A2021\INF8775\TPs\TP1-A21\data\ex8_1")
     #print(matrix_parser.matrix1)
     #print(matrix_parser.matrix2)
 
@@ -45,7 +46,7 @@ if __name__ == "__main__":
 
     if algorithm == "conv":
         result = conventional.multiply(matrix_parser.matrix1, matrix_parser.matrix2)
-        # result2 = strassen1.strassen(np.array(matrix_parser.matrix1), np.array(matrix_parser.matrix2))
+        # result2 = strassen.strassen(np.array(matrix_parser.matrix1), np.array(matrix_parser.matrix2))
         # print(result)
         # print(result2)
         # if (result == result2).all():
@@ -54,10 +55,10 @@ if __name__ == "__main__":
         #     print("REPONSE DIFFERENTE")
 
     if algorithm == "strassen":
-        result = strassen1.strassen(np.array(matrix_parser.matrix1), np.array(matrix_parser.matrix2))
+        result = strassen.strassen(np.array(matrix_parser.matrix1), np.array(matrix_parser.matrix2))
     
     if algorithm == "strassenSeuil":
-        #result1 = strassen1.strassen(np.array(matrix_parser.matrix1), np.array(matrix_parser.matrix2))
+        #result1 = strassen.strassen(np.array(matrix_parser.matrix1), np.array(matrix_parser.matrix2))
         result = strassen_sueil.strassenSeuil(np.array(matrix_parser.matrix1), np.array(matrix_parser.matrix2), 60)
 
     if show_result:
@@ -79,27 +80,29 @@ if __name__ == "__main__":
 # print(2**index)
     
 
-# results = []
-# for i in range(3,9):
-#     for j in range(1,5):
-#         matrix_parser.parse(rf"C:\Users\yanis\Desktop\Poly\A2021\INF8775\TPS\TP1-A21\data\ex{i}_0",rf"C:\Users\yanis\Desktop\Poly\A2021\INF8775\TPS\TP1-A21\data\ex{i}_{j}")
-#         print(f"Dimension: {i}, exemplaire: {0} multiply {j}")
-#         start = timer()
-#         result = conventional.multiply(matrix_parser.matrix1, matrix_parser.matrix2)
-#         end = timer()
-#         print("conv")
-#         print("--- %s seconds ---" % (end - start))
+results = []
+for i in range(3,9):
+    for j in range(1,5):
+        matrix_parser.parse(rf"C:\Users\yanis\Desktop\Poly\A2021\INF8775\TPS\TP1-A21\data\ex{i}_0",rf"C:\Users\yanis\Desktop\Poly\A2021\INF8775\TPS\TP1-A21\data\ex{i}_{j}")
+        #print(f"Dimension: {i}, exemplaire: {0} multiply {j}")
+        #start = timer()
+        #start = time.perf_counter()
+        #result = conventional.multiply(matrix_parser.matrix1, matrix_parser.#matrix2)
+        #end = time.perf_counter()
+        #end = timer()
+        #print("conv")
+        #print((end - start))
 
-#         start = timer()
-#         result = strassen1.strassen(np.array(matrix_parser.matrix1), np.array(matrix_parser.matrix2))
-#         end = timer()
-#         print("strassen")
-#         print("--- %s seconds ---" % (end - start))
+        # start = timer()
+        # result = strassen.strassen(np.array(matrix_parser.matrix1), np.array(matrix_parser.matrix2))
+        # end = timer()
+        # print("strassen")
+        # print("--- %s seconds ---" % (end - start))
 
-#         start = timer()
-#         result = strassen_sueil.strassenSeuil(np.array(matrix_parser.matrix1), np.array(matrix_parser.matrix2), 8)
-#         end = timer()
-#         print("strassen seuil")
-#         print("--- %s seconds ---" % (end - start))
-#         print()
+        # start = timer()
+        # result = strassen_sueil.strassenSeuil(np.array(matrix_parser.matrix1), np.array(matrix_parser.matrix2), 8)
+        # end = timer()
+        # print("strassen seuil")
+        # print("--- %s seconds ---" % (end - start))
+        # print()
 
--- TP1-A21/strassen_sueil.py
+++ TP1-A21/strassen_sueil.py
@@ -3,6 +3,7 @@
 
 import numpy as np
 import conventional
+import strassen
 
 def split(matrix):
     """
@@ -17,32 +18,4 @@ def split(matrix):
 def strassenSeuil(x, y, seuil):
        x = np.array(x)
        y = np.array(y)
-       if len(x) <=  seuil:
-              c = conventional.multiply(x,y)
-       else:
-              # Splitting the matrices into quadrants. This will be done recursively
-              # until the base case is reached.
-              a, b, c, d = split(x)
-              e, f, g, h = split(y)
-
-              # Computing the 7 products, recursively (p1, p2...p7)
-              p1 = strassenSeuil(a, f - h, seuil)
-              p2 = strassenSeuil(a + b, h, seuil)
-              p3 = strassenSeuil(c + d, e, seuil)
-              p4 = strassenSeuil(d, g - e, seuil)
-              p5 = strassenSeuil(a + d, e + h, seuil)
-              p6 = strassenSeuil(b - d, g + h, seuil)
-              p7 = strassenSeuil(a - c, e + f, seuil)
-
-              # Computing the values of the 4 quadrants of the final matrix c
-              c11 = p5 + p4 - p2 + p6
-              c12 = p1 + p2
-              c21 = p3 + p4
-              c22 = p1 + p5 - p3 - p7
-
-              # Combining the 4 quadrants into a single matrix by stacking horizontally and vertically.
-              c = np.vstack((np.hstack((c11, c12)), np.hstack((c21, c22))))
-
-              #print(c)
-
-       return c
+       return  conventional.multiply(x,y) if len(x) <= seuil else strassen.strassen(x,y)
