193bf8a4274d5ea7cf6faaf4bd104d2fd4624a89
Yacine <yacine.hamdani@polymtl.ca>
2021-11-12 09:06:35 +0800

    for pull

---
+++ TP2-A21/glouton.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import matrixparser
+import node
+
+matrix = matrixparser.parse("/Users/yacinehamdani/Desktop/SessionA21-etranger/cours/inf8775/TP1-A21/INF8775-TPs/TP2-A21/instances/ex5_0")
+node1 = node.Node(None, None, None)
+
+
+              
+
+
+def chooseFirstPoint():
+       node1.calculateDegree(matrix)
+
+
+node1.calculateDegree(matrix)
--- TP2-A21/matrixparser.py
+++ TP2-A21/matrixparser.py
@@ -3,8 +3,11 @@ import numpy as np
 
 
 def parse(matrix_path):
-    global matrix1, matrix2
-
+    global matrix1
     with open(matrix_path, "r") as matrix1_file:
         N1 = matrix1_file.readline()
         matrix1 = [list(map(int,line.split())) for line in matrix1_file] 
+    return matrix1
+
+    
+
