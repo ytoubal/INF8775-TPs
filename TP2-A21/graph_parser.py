302c3afe7c12e2b40bd82c69e6abeed7c86e6c60
Yanis Toubal <yanis.toubal@polymtl.ca>
2021-11-13 19:13:25 -0500

    added classes graph and nodes

--- TP2-A21/graph_parser.py
+++ TP2-A21/graph_parser.py
@@ -1,12 +1,55 @@
 #!/usr/bin/env python3
-import numpy as np
-import networkx as nx
-import matplotlib.pyplot as plt
+
+class Graph:
+
+    def __init__(self):
+        self.nodes = []
+
+    def add_node(self, node):
+        self.nodes.append(node)
+
+    def add_edge(self, node1_id, node2_id):
+        #Index is id-1 since id start at 1
+        node1 = self.nodes[node1_id-1]
+        node2 = self.nodes[node2_id-1]
+        node1.add_neighbor(node2)
+        node2.add_neighbor(node1)     
+
+    def show_graph(self):
+        for node in self.nodes:
+            print("Node: " + node.id)
+            print("Color: " + str(node.color))
+            print("Degree: " + str(node.get_degree()))
+            print("Neighbors: " + str([node.id for node in node.neighbors]))
+            print()
+
+class Node:
+    
+    def __init__(self, id, color = -1):
+        self.id = id
+        self.color = color
+        self.neighbors = []
+
+    def set_color(self, color):
+        self.color = color
+
+    def add_neighbor(self, node):
+        self.neighbors.append(node)
+    
+    def remove_neighbor(self, node):
+        self.neighbors.remove(node)
+
+    def get_degree(self):
+        return len(self.neighbors)
+
+    def find_DSAT(self):
+        return sum(1 for node in self.neighbors if node.color != -1)
+
 
 def parse(matrix_path):
     global graph
 
-    graph = nx.Graph()
+    graph = Graph()
 
     with open(matrix_path, "r") as matrix_file:
         #Skip 11 first lines which arent important
@@ -15,22 +58,20 @@ def parse(matrix_path):
             next(matrix_file)
         info_graph = matrix_file.readline().split(' ')
         number_node = info_graph[2]
-        number_edge = info_graph[3]
-        # print(number_vertex)
-        # print(number_edge)
-        graph.add_nodes_from([str(i+1) for i in range(int(number_node))], color=-1) # no color for each node
+
+        for i in range(int(number_node)):
+            graph.add_node(Node(str(i+1))) #Nodes start at 1
+        
         next(matrix_file)
 
         for line in matrix_file: 
             info_edge = line.strip('\n').split(' ')
-            edge1 = info_edge[1]
-            edge2 = info_edge[2]
+            edge1 = int(info_edge[1])
+            edge2 = int(info_edge[2])
             # print(info_edge)
             graph.add_edge(edge1, edge2)
-        # print(graph.nodes)
-        # print(graph.edges)
-        nx.draw_networkx(graph, with_labels=True)
-        #plt.show()
-    
+        
+        #graph.show_graph()
+
 
 
