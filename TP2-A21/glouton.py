7a32fcf664b723cce1e7bbbeb51d9e731d073769
Yanis Toubal <yanis.toubal@polymtl.ca>
2021-11-13 19:27:22 -0500

    refactored glouton with new data structure

--- TP2-A21/glouton.py
+++ TP2-A21/glouton.py
@@ -1,45 +1,40 @@
 #!/usr/bin/env python3
-import networkx as nx
-import matplotlib.pyplot as plt
 
-def check_nodes_colored(graph: nx.Graph):
+def check_nodes_colored(graph):
     for node in graph.nodes:
-        if graph.nodes[node]["color"] == -1:
+        if node.color == -1:
             return False
     return True
 
-def find_DSAT(graph: nx.Graph):
+def find_DSAT(graph):
     max_dsat, max_dsat_node = -1, None
     for node in graph.nodes:
-        if graph.nodes[node]["color"] != -1:
+        if node.color != -1:
             continue
         dsat = 0
-        for adj_node in graph.neighbors(node):
-            if graph.nodes[adj_node]["color"] != -1:
+        for adj_node in node.neighbors:
+            if adj_node.color != -1:
                 dsat += 1
         if dsat > max_dsat:
             max_dsat = dsat
-            max_dsat_node = node   # À voir si on garde node
+            max_dsat_node = node   
             
     return max_dsat_node
 
-def find_smallest_color(node, graph: nx.graph, current_color = 0):
-    colors_taken = []   
-    for adj_node in graph.neighbors(node):
-        colors_taken.append(graph.nodes[adj_node]["color"])
-        if graph.nodes[adj_node]["color"] == current_color:
+def find_smallest_color(node, graph, current_color = 0):
+    for adj_node in node.neighbors:
+        if adj_node.color == current_color:
             current_color = find_smallest_color(node, graph, current_color + 1)
     return current_color
 
-def find_colors(graph: nx.Graph):
-    max_degree_node = max(graph.degree, key=lambda x: x[1])[0] # v
-    graph.nodes[max_degree_node]["color"] = 0 # C[v] = 0
+def find_colors(graph):
+    nodes_degree = [node.get_degree() for node in graph.nodes]
+    max_degree_node = nodes_degree.index(max(nodes_degree)) # v
+    graph.nodes[max_degree_node].set_color(0) # C[v] = 0
 
     while not check_nodes_colored(graph):
         chosen_node = find_DSAT(graph)
         color = find_smallest_color(chosen_node, graph)
-        graph.nodes[chosen_node]["color"] = color
+        chosen_node.set_color(color)
 
-    #print(graph.nodes.data())
-    #plt.show()
-    return [graph.nodes[node]["color"] for node in graph.nodes]
+    return [node.color for node in graph.nodes]
